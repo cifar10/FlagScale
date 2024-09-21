0879add8f77a90d41069b6331d4a94bfb253401b
cifar10 <731376883@qq,com>
2024-09-21 18:00:01 +0800

    test test

--- megatron/megatron/core/tensor_parallel/layers.py
+++ megatron/megatron/core/tensor_parallel/layers.py
@@ -227,13 +227,17 @@ class VocabParallelEmbedding(torch.nn.Module):
             masked_input[input_mask] = 0
         else:
             masked_input = input_
+
         # Get the embeddings.
         if self.deterministic_mode:
-            output_parallel = self.weight[masked_input]
+            output_parallel = F.embedding(
+                masked_input, self.weight, padding_idx=None, scale_grad_by_freq=False, sparse=False)
         else:
             # F.embedding currently has a non-deterministic backward function
-            output_parallel = F.embedding(masked_input, self.weight)
+            output_parallel = F.embedding(
+                masked_input, self.weight, padding_idx=None, scale_grad_by_freq=False, sparse=False)
         # Mask the output embedding.
+
         if self.tensor_model_parallel_size > 1:
             output_parallel[input_mask, :] = 0.0
 
